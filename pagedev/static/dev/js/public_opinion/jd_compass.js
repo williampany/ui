e1482279eeb657e253d709a6890e8d7ebfef2664
shanghongliang <shanghongliang@91jinrong.com>
2015-08-30 09:16:03 +0800

    1.京东罗盘页面样式完善

---
+++ pagedev/static/dev/js/public_opinion/jd_compass.js
@@ -0,0 +1,14 @@
+// description：京东罗盘页面脚本
+// author:vicshang(2015-8-29 19:12:48)
+// update
+var JdCompass=function(){}
+JdCompass.prototype={
+	//初始化函数
+	init:function(){
+		this.initBind();
+	},
+	//事件绑定初始化事件
+	initBind:function(){
+
+	},
+}
